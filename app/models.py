5d6ab93830b060425982a51bb09a46e0da67be9c
Monica2495 <monrpadilla@misena.edu.co>
2024-02-27 13:07:06 -0600

    entidades

--- app/models.py
+++ app/models.py
@@ -11,3 +11,13 @@ class Medico(db.Model):
     numero_identificacion = db.Column(db.Integer)
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
+
+class Paciente(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120), nullable = True)
+    apellidos = db.Column(db.String(120), nullable = True)
+    tipo_identificacion = db.Column(db.String(3), nullable = True)
+    numero_identificacion = db.Column(db.Integer)
+    altura = db.Column(db.Integer)
+    tipo_sangre =db.Column(db.String(2))
